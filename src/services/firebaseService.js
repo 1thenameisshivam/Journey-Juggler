1d8a33a9c66dfb348960a80b398058f7d80ffdc8
1thenameisshivam <1thenameisshivam@gmail.com>
2024-07-20 21:19:23 +0530

    env configured

--- src/services/firebaseService.js
+++ src/services/firebaseService.js
@@ -1,17 +1,22 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import {
+  VITE_FIREBASE_API_KEY,
+  VITE_FIREBASE_APPID,
+  VITE_MESSAGESENDERID,
+} from "@/utils/constent";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCluijk9bpvZaweYsNYxOZJdaZ_D57OUJo",
+  apiKey: VITE_FIREBASE_API_KEY,
   authDomain: "journey-juggler.firebaseapp.com",
   projectId: "journey-juggler",
   storageBucket: "journey-juggler.appspot.com",
-  messagingSenderId: "900870138785",
-  appId: "1:900870138785:web:371723fdc4238be4e9b9e0",
+  messagingSenderId: VITE_MESSAGESENDERID,
+  appId: VITE_FIREBASE_APPID,
 };
 
 // Initialize Firebase
