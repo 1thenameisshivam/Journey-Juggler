e3bbc8fe022e127f4a6715ddadae18cb2aadfb5a
1thenameisshivam <1thenameisshivam@gmail.com>
2024-09-03 01:45:44 +0530

    small bug fix

--- src/components/custom/Logo.jsx
+++ src/components/custom/Logo.jsx
@@ -1,7 +1,7 @@
 const Logo = () => {
   return (
     <div className="flex items-center">
-      <span className="text-sm md:text-2xl font-serif "> Jurney</span>
+      <span className="text-sm md:text-2xl font-serif "> Journey</span>
       <span className="text-lg md:text-3xl"> 🪂</span>
       <span className="text-sm font-serif md:text-2xl"> Juggler</span>
     </div>
